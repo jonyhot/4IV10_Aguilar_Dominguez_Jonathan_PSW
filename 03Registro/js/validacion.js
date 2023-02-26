6cbe491840f71c3afc88083c254abc852cebbf75
jonyhot <jaguilard2100@alumno.ipn.mx>
2023-02-25 22:46:05 -0600

    formulario

--- 03Registro/js/validacion.js
+++ 03Registro/js/validacion.js
@@ -1,20 +1,67 @@
+/*
+JavaScript es un lenguaje
+que posee un paradigma orientado a objetos, funciones
+y eventos.
+Por tal motivo presenta una particularidad que es:
+
+NO TIPADO
+No existe int, float, straing, ni char, ni nada :((
+
+Todo es var -> variable
+
+De acuerdo al estándar ES6 se manejan 3 tipos de variable
+
+VAR es una variable global
+LET es una variable del tipo "Protected"
+CONST es un valor constante
+*/
+
 const nombre = document.getElementById("myname");
-const edad = document.getElementById("edad");
-const fechadenacimiento = getElementById("nacimiento");
 const correo = document.getElementById("email");
-const condiciones = Document.getElementById("condiciones");
+const terminosYcondiciones = document.getElementById("termsAndConditions");
 const form = document.getElementById("form");
-const listInputs = document.querySelectorAll(".form-input ");
+const listInputs = document.querySelectorAll(".form-input");
 
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-
-    if (nombre.ariaValueMax.length < 1 || nombre.ariaValueMax.trim() == ""){
-        mostrarMensajeError();
-    }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let condicion = validacionForm();
+  if (condicion) {
+    enviarFormulario();
+  }
 });
 
-function mostrarMensajeError(){
-    let elemento = document.querySelector(".myname");
-    elemento.lastElementChild.innnerHTML = "Nombre no valido";
+function validacionForm() {
+  form.lastElementChild.innerHTML = "";
+  let condicion = true;
+  listInputs.forEach((element) => {
+    element.lastElementChild.innerHTML = "";
+  });
+
+  if (nombre.value.length < 1 || nombre.value.trim() == "") {
+    mostrarMensajeError("myname", "Nombre no valido*");
+    condicion = false;
+  }
+
+  if (correo.value.length < 1 || correo.value.trim() == "") {
+    mostrarMensajeError("email", "Correo no valido*");
+    condicion = false;
+  }
+
+  if (!terminosYcondiciones.checked) {
+    mostrarMensajeError("termsAndConditions", "Acepte*");
+    condicion = false;
+  } else {
+    mostrarMensajeError("termsAndConditions", "");
+  }
+  return condicion;
+}
+
+function mostrarMensajeError(claseInput, mensaje) {
+  let elemento = document.querySelector(`.${claseInput}`);
+  elemento.lastElementChild.innerHTML = mensaje;
+}
+
+function enviarFormulario() {
+  form.reset();
+  form.lastElementChild.innerHTML = "Listo !!";
 }
